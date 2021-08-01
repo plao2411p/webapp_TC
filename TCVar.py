fba492c71ca2eae41a37563a92c26db853861e1e
Pedro-Luis <plao2411p@gmail.com>
2021-08-01 16:38:51 -0500

    Variables globales

---
+++ TCVar.py
@@ -0,0 +1,48 @@
+
+import numpy as np
+import pandas as pd
+import sys
+from os import system, name
+
+# función para limpiar terminal
+
+
+def clear():
+
+    # para windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # para mac y linux
+    else:
+        _ = system('clear')
+
+
+Operadores = "unindids"
+Conjuntos = "UABC"
+
+# Conjuntos
+U = np.arange(1, 10, 1)
+# Los conjuntos A, B y C deben estar dentro de U
+A = np.arange(1, 5, 1)
+B = np.arange(2, 10, 2)
+C = np.arange(3, 7, 1)
+
+# Númer de operaciones
+N_Un = 0  # uniones
+N_I = 0  # intersecciones
+N_D = 0  # diferencias
+N_DS = 0  # diferencias simétricas
+N_Total = 0  # total de operaciones
+N_PP = 0  # Numero de pares de parentesis
+
+
+def SolicitarConjunto(cadena):
+    if 'A' in cadena:
+        return A
+    elif 'B' in cadena:
+        return B
+    elif 'C' in cadena:
+        return C
+    elif 'U' in cadena:
+        return U
